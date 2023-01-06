f8b7aeb7198cf620cc2332dc1b36fa42517186de
Ali AYTEKİN <96261457+aytek55@users.noreply.github.com>
2023-01-06 21:05:33 +0300

    Layers were written for the product table and tried with postman.

---
+++ DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=ALIAYTEKIN\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+       
+    }
+}
